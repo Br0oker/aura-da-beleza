074a905b122fee6e4c5d7a1d2b42c2224bbf855d
Aura da Beleza <85753429+Br0oker@users.noreply.github.com>
2023-05-29 00:41:05 -0300

    app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Obtenha os dados do formulário
+        nome = request.form['nome']
+        email = request.form['email']
+
+        # Faça algo com os dados (por exemplo, salvar em um arquivo)
+        with open('dados.txt', 'a') as arquivo:
+            arquivo.write(f"Nome: {nome}\nEmail: {email}\n\n")
+
+        return 'Obrigado por enviar o formulário!'
+    
+    # Se não houver solicitação POST, exiba o formulário
+    return render_template('formulario.html')
+
+if __name__ == '__main__':
+    app.run()
